f8b13f062f6e2b1b4a027ee26e1d1a039b21dedc
lucianomc <luu.mcosta@gmail.com>
2019-06-29 20:19:24 -0300

    Adding clean date in TaskModelForm

--- todo_list/forms.py
+++ todo_list/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.forms import ModelForm
+from datetime import datetime, date
 
 from .models import Tasks
 
@@ -18,3 +19,9 @@ class TaskModelForm(forms.ModelForm):
             'priority': forms.Select(
                 attrs={'class': 'form-control'}),
         }
+
+    def clean_finish_in(self):
+        finish_in = self.cleaned_data['finish_in']
+        if finish_in < date.today():
+            raise forms.ValidationError("Data inválida")
+        return finish_in
